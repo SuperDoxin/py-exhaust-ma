ab0c373704c89ff041f248ad439504886c92a9d5
Doxin <lieuwemo@gmail.com>
2020-02-20 13:27:15 +0100

    Add from_file and from_lines to Warrior
    This enables compiling warriors fully in-memory, without involving
    any files.

--- exhaust_ma/tests/test_simple_warriors.py
+++ exhaust_ma/tests/test_simple_warriors.py
@@ -1,5 +1,6 @@
 from .. import Warrior, Core, CoreSettings
 from pkg_resources import resource_filename
+import pytest
 
 
 def test_stalker_vs_imp():
@@ -9,6 +10,74 @@ def test_stalker_vs_imp():
     cs = CoreSettings()
     w1 = Warrior.from_filename(cs, stalker)
     w2 = Warrior.from_filename(cs, imp)
+
+    c = Core(cs)
+    c.load_warriors([w1, w2])
+
+    result = c.run()
+
+    assert w1 in result.alive
+    assert w1 not in result.dead
+
+    assert w2 not in result.alive
+    assert w2 in result.dead
+
+
+@pytest.mark.parametrize(
+    "warrior_filename",
+    [
+        resource_filename("exhaust_ma", "exhaust-ma/stalker.rc"),
+        resource_filename("exhaust_ma", "exhaust-ma/jaguar.rc"),
+        resource_filename("exhaust_ma", "exhaust-ma/spl.rc"),
+        resource_filename("exhaust_ma", "exhaust-ma/imp.rc"),
+        resource_filename("exhaust_ma", "exhaust-ma/t/ptest4.rc"),
+        resource_filename("exhaust_ma", "exhaust-ma/t/ptest6.rc"),
+        resource_filename("exhaust_ma", "exhaust-ma/t/pin1a.rc"),
+        resource_filename("exhaust_ma", "exhaust-ma/t/ptest5.rc"),
+        resource_filename("exhaust_ma", "exhaust-ma/t/validate.rc"),
+        resource_filename("exhaust_ma", "exhaust-ma/t/pin1b.rc"),
+        resource_filename("exhaust_ma", "exhaust-ma/t/ptest3.rc"),
+        resource_filename("exhaust_ma", "exhaust-ma/t/pin2b.rc"),
+        resource_filename("exhaust_ma", "exhaust-ma/t/pin2a.rc"),
+        resource_filename("exhaust_ma", "exhaust-ma/t/ptest1.rc"),
+        resource_filename("exhaust_ma", "exhaust-ma/t/ptest2.rc"),
+        resource_filename("exhaust_ma", "exhaust-ma/stalker.rc"),
+        resource_filename("exhaust_ma", "exhaust-ma/fixed.rc"),
+        resource_filename("exhaust_ma", "exhaust-ma/npaper2.rc"),
+    ],
+)
+def test_from_file(warrior_filename):
+    cs = CoreSettings()
+    with open(warrior_filename) as fp:
+        w1 = Warrior.from_file(cs, fp)
+
+    w2 = Warrior.from_filename(cs, warrior_filename)
+
+    assert w1.warrior_pt.start == w2.warrior_pt.start
+    assert w1.warrior_pt.have_pin == w2.warrior_pt.have_pin
+    assert w1.warrior_pt.pin == w2.warrior_pt.pin
+    assert len(w1) == len(w2)
+
+    for op1, op2 in zip(
+        w1.warrior_pt.code[0 : len(w1)], w2.warrior_pt.code[0 : len(w2)]
+    ):
+        assert getattr(op1, "in") == getattr(op2, "in")
+        assert op1.a == op2.a
+        assert op1.b == op2.b
+
+
+def test_stalker_vs_imp_from_file():
+    stalker = resource_filename("exhaust_ma", "exhaust-ma/stalker.rc")
+    imp = resource_filename("exhaust_ma", "exhaust-ma/imp.rc")
+
+    cs = CoreSettings()
+    with open(stalker) as fp:
+        w1 = Warrior.from_file(cs, fp)
+    w1.name = "stalker"
+
+    with open(imp) as fp:
+        w2 = Warrior.from_file(cs, fp)
+    w2.name = "imp"
 
     c = Core(cs)
     c.load_warriors([w1, w2])
--- exhaust_ma/warrior.py
+++ exhaust_ma/warrior.py
@@ -66,6 +66,14 @@ class Mode(Enum):
         assert False
 
 
+class AsmLine(Enum):
+    PIN = -3
+    ORG = -2
+    DONE = -1
+    NONE = 0
+    OK = 1
+
+
 class Warrior:
     def __init__(self, warrior_pt):
         self.warrior_pt = warrior_pt
@@ -123,4 +131,45 @@ class Warrior:
         )
         instance = cls(warrior_pt)
         instance.name = os.path.basename(filename)
+        return instance
+
+    @classmethod
+    def from_file(cls, core_settings, fp):
+        return cls.from_lines(core_settings, fp)
+
+    @classmethod
+    def from_lines(cls, core_settings, lines):
+        warrior_pt = _exhaust_ma.ffi.new("warrior_t*")
+        i = 0
+        for line in lines:
+            instruction_result = _exhaust_ma.ffi.new("insn_t*")
+            result = AsmLine(
+                _exhaust_ma.lib.asm_line(
+                    line.encode("utf-8"), instruction_result, core_settings.core_size
+                )
+            )
+            if result == AsmLine.OK:
+                warrior_pt.code[i] = {
+                    k: getattr(instruction_result, k) for k in ["in", "a", "b"]
+                }
+                if getattr(instruction_result, "in") & 0b0100000000000000:
+                    warrior_pt.start = i
+                i += 1
+            elif result == AsmLine.NONE:
+                pass
+            elif result == AsmLine.DONE:
+                break
+            elif result == AsmLine.ORG:
+                warrior_pt.start = instruction_result.a
+            elif result == AsmLine.PIN:
+                warrior_pt.pin = instruction_result.a
+                warrior_pt.have_pin = True
+            else:
+                assert False, "Unexpected return value from asm_line: {!r}".format(
+                    result
+                )
+
+        warrior_pt.len = i
+
+        instance = cls(warrior_pt)
         return instance
