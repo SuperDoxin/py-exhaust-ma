d4712bf9d08ddc1128e11dc72951b90ef412d416
Doxin <lieuwemo@gmail.com>
2021-07-02 16:53:30 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="exhaust-ma",
-    version="0.0.2",
+    version="0.0.3",
     setup_requires=["cffi>=1.0.0", "wheel>=0.34.2"],
     cffi_modules=["exhaust_ma/build_cffi.py:ffi"],
     install_requires=["cffi>=1.0.0", "wheel>=0.34.2"],
